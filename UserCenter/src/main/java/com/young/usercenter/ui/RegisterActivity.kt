8e9a75f1daf5f58534ba4350bfc937176ec68d09
yangke <yangke642@gmail.com>
2019-09-10 20:42:25 +0800

    user module

---
+++ UserCenter/src/main/java/com/young/usercenter/ui/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.young.usercenter.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.young.usercenter.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
